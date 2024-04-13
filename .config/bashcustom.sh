0224902113000bb3ef4c8a623974f84f55599820
Seamus <m0ose01@proton.me>
2024-04-13 16:50:22 +0100

    Add bash config files

---
+++ .config/bashcustom.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+################################################################
+# MANUALLY ENABLED BY SOS: 
+# Case-insensitive bash autocomplete.##
+################################################################
+bind 'set completion-ignore-case on'
+
+# FZF setup
+export FZF_DEFAULT_COMMAND=fd
+source /usr/share/doc/fzf/examples/key-bindings.bash
+alias nvsearch='nvim "$(fzf -i)"'
+alias fzf='fzf -i'
+###############################################################
