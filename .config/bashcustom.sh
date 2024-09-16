f9f461251a50e58bc032d5d026fbeab5d8c48f41
Seamus <m0ose01@proton.me>
2024-09-16 11:27:34 +0100

    Add nvim to path via bob.

--- .config/bashcustom.sh
+++ .config/bashcustom.sh
@@ -11,4 +11,6 @@ export FZF_DEFAULT_COMMAND=fd
 source /usr/share/doc/fzf/examples/key-bindings.bash
 alias nvsearch='nvim "$(fzf -i)"'
 alias fzf='fzf -i'
+
+export PATH="~/.local/share/bob/nvim-bin/:$PATH"
 ###############################################################
